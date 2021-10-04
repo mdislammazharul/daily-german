bd8e1cd7e2934f745a7b0baed6b98be3143fd5e6
mdislammazharul <mazharul.py@gmail.com>
2021-10-04 17:55:40 +0600

    enrollment added

--- src/components/Enrollment/Enrollment.js
+++ src/components/Enrollment/Enrollment.js
@@ -8,35 +8,51 @@ const Enrollment = () => {
                 <h1 className="home-heading mt-5 mb-5">Enrollment Form</h1>
                 <Form>
                     <Row className="mb-3">
+                        <Form.Label><strong>Select Desired Service</strong></Form.Label>
+                        <Form.Select className="mb-3">
+                            <option>Basic</option>
+                            <option>Medium</option>
+                            <option>Advanced</option>
+                            <option>Premium</option>
+                            <option>Professional</option>
+                            <option>A1</option>
+                            <option>A2</option>
+                            <option>B1</option>
+                            <option>B2</option>
+                            <option>C1</option>
+                            <option>C2</option>
+                            <option>None of this</option>
+                        </Form.Select>
+
                         <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Label>Email</Form.Label>
+                            <Form.Label><strong>Email</strong></Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
-                            <Form.Label>Password</Form.Label>
+                            <Form.Label><strong>Password</strong></Form.Label>
                             <Form.Control type="password" placeholder="Password" />
                         </Form.Group>
                     </Row>
 
                     <Form.Group className="mb-3" controlId="formGridAddress1">
-                        <Form.Label>Address</Form.Label>
+                        <Form.Label><strong>Permanent Address</strong></Form.Label>
                         <Form.Control placeholder="1234 Main St" />
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formGridAddress2">
-                        <Form.Label>Address 2</Form.Label>
+                        <Form.Label><strong>Current Address</strong></Form.Label>
                         <Form.Control placeholder="Apartment, studio, or floor" />
                     </Form.Group>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridCity">
-                            <Form.Label>City</Form.Label>
+                            <Form.Label><strong>City</strong></Form.Label>
                             <Form.Control />
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridState">
-                            <Form.Label>State</Form.Label>
+                            <Form.Label><strong>State</strong></Form.Label>
                             <Form.Select defaultValue="Choose...">
                                 <option>Choose...</option>
                                 <option>...</option>
@@ -44,7 +60,7 @@ const Enrollment = () => {
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridZip">
-                            <Form.Label>Zip</Form.Label>
+                            <Form.Label><strong>Zip</strong></Form.Label>
                             <Form.Control />
                         </Form.Group>
                     </Row>
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './NotFound.css'
+import img from '../../images/404.jpg'
 
 const NotFound = () => {
     return (
         <div>
-            <h1>This is Not found</h1>
+            <h1 className="home-heading mt-5">Sorry!</h1>
+            <h1>This Page Cannot Be Found!</h1>
+            <img src={img} alt="" />
         </div>
     );
 };
